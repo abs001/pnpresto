14c86773906986e71b2dfe7b1fed96f225a36f14
Abhijit Kumbhar <abs001>
2020-07-04 23:40:18 +0530

    Completed Model Design

--- resto/admin.py
+++ resto/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SiteConfiguration, Menu, MenuType, Contact, Reservation
 
-# Register your models here.
+admin.site.register(SiteConfiguration)
+admin.site.register(Menu)
+admin.site.register(MenuType)
+admin.site.register(Contact)
+admin.site.register(Reservation)
--- resto/models.py
+++ resto/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class SiteConfiguration(models.Model):
+    site_logo = models.CharField()
+    site_name = models.CharField()
+    site_info = models.TextField()
+    banner_image1 = models.ImageField()
+    banner_image2 = models.ImageField()
+    banner_image3 = models.ImageField()
+    # Contact Details
+    about = models.CharField()
+    about_description = models.TextField()
+    phone_number = models.CharField()
+    email = models.EmailField()()
+    city = models.CharField()
+    address = models.TextField()
+    # Google Map API Integration
+    google_map_key = models.CharField()
+    google_map_latitude = models.CharField()
+    google_map_longitude = models.CharField()
+    # Social Links
+    facebook = models.CharField()
+    twitter = models.CharField()
+    instagram = models.CharField()
+    linkedin = models.CharField()
+
+
+class MenuType(models.Model):
+    type = models.CharField(primary_key=True)
+
+
+class Menu(models.Model):
+    menu_name = models.CharField()
+    menu_type = models.ForeignKey(MenuType, on_delete=models.CASCADE())
+    menu_price = models.FloatField()
+    menu_description = models.TextField()
+    menu_photo = models.ImageField()
+
+
+class Contact(models.Model):
+    name = models.CharField()
+    email_id = models.EmailField()
+    mobile_no = models.CharField()
+    message = models.CharField()
+
+
+class Reservation(models.Model):
+    reservation_date = models.DateField()
+    reservation_from = models.DateTimeField()
+    reservation_to = models.DateTimeField()
+    total_persons = models.IntegerField()
+    name = models.CharField()
+    mobile_no = models.CharField()
+    email_id = models.EmailField()
