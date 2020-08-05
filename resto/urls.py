76836228f38975b33da40afae6cadc44eed91ffb
Abhijit Kumbhar <abs001>
2020-08-06 00:13:55 +0530

    Added separate menu page and fixed few layout of template

--- resto/urls.py
+++ resto/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # url(r'^resto/menus/$', views.menus, name='menus')
+    path('menus/', views.menus, name='menus'),
 ]
--- resto/views.py
+++ resto/views.py
@@ -7,20 +7,24 @@ def index(request):
     context = {}
     site_configuration = SiteConfiguration.objects.get()
     context.update({
-        "banners": [site_configuration.banner_image1, site_configuration.banner_image2, site_configuration.banner_image3],
-        "site_details": {
-            "site_name": site_configuration.site_name,
-            "site_logo": site_configuration.site_logo,
-            "site_info": site_configuration.site_info
-        },
-        "about_details": {
-            "about": site_configuration.about,
-            "about_description": site_configuration.about_description
-        },
-        "menu_type": MenuType.objects.all(),
-        "menus": Menu.objects.all(),
         "gallery": Gallery.objects.all(),
-        "customer_review": CustomerReview.objects.all(),
+        "customer_review": CustomerReview.objects.all()
+    })
+    context.update(get_common())
+    context.update(get_menu_details())
+    return render(request, "resto/home.html", context)
+
+
+def menus(request):
+    context = {}
+    context.update(get_common())
+    context.update(get_menu_details())
+    return render(request, "resto/menus.html", context)
+
+
+def get_common():
+    site_configuration = SiteConfiguration.objects.get()
+    return {
         "phone": site_configuration.phone_number,
         "email": site_configuration.email,
         "city": site_configuration.city,
@@ -30,6 +34,23 @@ def index(request):
             "twitter": site_configuration.twitter,
             "instagram": site_configuration.instagram,
             "linkdin": site_configuration.linkedin
-        }
-    })
-    return render(request, "resto/home.html", context)
+        },
+        "site_details": {
+            "site_name": site_configuration.site_name,
+            "site_logo": site_configuration.site_logo,
+            "site_info": site_configuration.site_info
+        },
+        "about_details": {
+            "about": site_configuration.about,
+            "about_description": site_configuration.about_description
+        },
+        "banners": [site_configuration.banner_image1, site_configuration.banner_image2,
+                    site_configuration.banner_image3],
+    }
+
+
+def get_menu_details():
+    return {
+        "menu_type": MenuType.objects.all(),
+        "menus": Menu.objects.all(),
+    }
